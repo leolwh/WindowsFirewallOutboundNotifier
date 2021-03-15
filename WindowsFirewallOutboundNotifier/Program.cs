fccde16b74846b8381cada6bd52f6a303adc53af
leolw <leolw@outlook.com>
2021-03-15 23:20:47 +1300

    enhance performance to reduce cpu/memory and battery usage

--- WindowsFirewallOutboundNotifier/Program.cs
+++ WindowsFirewallOutboundNotifier/Program.cs
@@ -10,7 +10,6 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
-using WindowsFirewallHelper;
 
 namespace WindowsFirewallOutboundNotifier
 {
@@ -35,11 +34,17 @@ namespace WindowsFirewallOutboundNotifier
         private static extern uint QueryDosDevice(string lpDeviceName, StringBuilder lpTargetPath, uint ucchMax);
         public static Dictionary<string, string> diskMap = null;
 
-        private static List<INetFwRule> filteredRules = new List<INetFwRule>();
+        public static Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+        public static INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
+        //bool varible for preventing duplicate notification
+        public static bool displayNotification = true;
+
+        private static List<INetFwRule> filteredRules = null;
         static void Main(string[] args)
         {
             Magician.DisappearConsole();
- 
+
+            Program exe = new Program();
             ReadAllFirewallRules();
 
             EventLog securityLogs = new EventLog("Security");
@@ -51,23 +56,20 @@ namespace WindowsFirewallOutboundNotifier
 
         private static void ReadAllFirewallRules()
         {
-            filteredRules.Clear();
-            Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
-            INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
-            List<INetFwRule> allRules = new List<INetFwRule>();
-            foreach (INetFwRule ruleItem in fwPolicy2.Rules)
+            if (filteredRules != null)
             {
-                allRules.Add(ruleItem);
+                filteredRules.Clear();
             }
-            filteredRules = allRules.Where(x => x.Direction == NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT).ToList();
-            fwPolicy2 = null;
-            tNetFwPolicy2 = null;
+
+            filteredRules = fwPolicy2.Rules.Cast<INetFwRule>().Where(x => x.Direction == NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT && x.Name.EndsWith("出站连接")).ToList();
         }
 
         private static void OnEntryWritten(object source, EntryWrittenEventArgs e)
         {
+            //sleep 1 second to optism performance
+            Thread.Sleep(1000);
             List<EventRecord> filteredEntries = new List<EventRecord>();
-            string eventFilterQuery = "*[System/EventID=5152 or System/EventID=5157]";
+            string eventFilterQuery = "*[System[(EventID=5152 or EventID=5157) and TimeCreated[timediff(@SystemTime) <= 180000]]]";
             EventLogQuery eventsQuery = new EventLogQuery("Security", PathType.LogName, eventFilterQuery);
             try
             {
@@ -76,7 +78,7 @@ namespace WindowsFirewallOutboundNotifier
                 {
                     filteredEntries.Add(eventdetail);
                 }
-             
+
             }
             catch (EventLogNotFoundException)
             {
@@ -86,68 +88,71 @@ namespace WindowsFirewallOutboundNotifier
             {
                 var filePath = GetFriendlyPath(query.Properties[1].Value.ToString());
                 var preRuleNameComponent = filePath.Split('\\');
-                if (preRuleNameComponent.Length < 2)
+                if (preRuleNameComponent.Length >= 2)
                 {
-                    return;
-                }
-                var ruleNameComponent = preRuleNameComponent[preRuleNameComponent.Length - 1];
+                    var ruleNameComponent = preRuleNameComponent[preRuleNameComponent.Length - 1];
 
-                var existRule = filteredRules.Any(x => x.Name == "允许 " + ruleNameComponent + " 出站连接" || x.Name == "阻止 " + ruleNameComponent + " 出站连接");
-                if (existRule == false)
-                {
-                    new ToastContentBuilder()
-                                        .AddText("发现新的出站连接请求")
-                                        .AddText(filePath)
-                                        .AddText("请求建立出站连接")
-                                        .AddButton(new ToastButton().SetContent("允许").AddArgument("action", "AllowConnection"))
-                                        .AddButton(new ToastButton().SetContent("阻止").AddArgument("action", "BlockConnection"))
-                                        .Show();
-
-                    ToastNotificationManagerCompat.OnActivated += toastArgs =>
+                    var existRule = filteredRules.Any(x => x.Name.EndsWith(ruleNameComponent + " 出站连接"));
+                    if (existRule == false)
                     {
-                        ToastArguments args = ToastArguments.Parse(toastArgs.Argument);
+                        if (displayNotification)
+                        {
+                            new ToastContentBuilder()
+                                            .AddText("发现新的出站连接请求")
+                                            .AddText(filePath)
+                                            .AddText("请求建立出站连接")
+                                            .AddButton(new ToastButton().SetContent("允许").AddArgument("action", "AllowConnection"))
+                                            .AddButton(new ToastButton().SetContent("阻止").AddArgument("action", "BlockConnection"))
+                                            .Show();
+                            displayNotification = false;
+                            ToastNotificationManagerCompat.OnActivated += toastArgs =>
+                            {
+                                ToastArguments args = ToastArguments.Parse(toastArgs.Argument);
 
-                        FirewallActions(args, filePath, ruleNameComponent);
-                        args = null;
-                    };
+                                FirewallActions(args, filePath, ruleNameComponent);
+                                args = null;
+                            };
+                        }
+                    }
                 }
 
             }
-            //sleep 1 second to optism performance
-            Thread.Sleep(1000);
+         
         }
 
         private static void FirewallActions(ToastArguments args, string fileName, string ruleNameComponent)
         {
             var buttonSelection = args.First().Value;
-            if (buttonSelection == "AllowConnection")
+            INetFwRule2 firewallRule = (INetFwRule2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            INetFwPolicy2 operatePolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            var existRule = filteredRules.Any(x => x.Name.EndsWith(ruleNameComponent + " 出站连接"));
+            if (existRule == false)
             {
-                var existRule = filteredRules.Any(x => x.Name == "允许 " + ruleNameComponent + " 出站连接");
-                if (existRule == false)
+                if (buttonSelection == "AllowConnection")
                 {
-                    var allowedRule = FirewallManager.Instance.CreateApplicationRule(
-                                       FirewallManager.Instance.GetProfile().Type, @"允许 " + ruleNameComponent + " 出站连接",
-                                       FirewallAction.Allow, fileName);
-                    allowedRule.Direction = FirewallDirection.Outbound;
-                    allowedRule.Protocol = FirewallProtocol.Any;
-                    FirewallManager.Instance.Rules.Add(allowedRule);
+                    var currentProfiles = fwPolicy2.CurrentProfileTypes;
+                    firewallRule.Enabled = true;
+                    firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+                    firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
+                    firewallRule.ApplicationName = fileName;
+                    firewallRule.Name = "允许 " + ruleNameComponent + " 出站连接";
+                    firewallRule.Profiles = currentProfiles;
+                    operatePolicy.Rules.Add(firewallRule);
                 }
-
-            }
-            else
-            {
-                var existRule = filteredRules.Any(x => x.Name == "阻止 " + ruleNameComponent + " 出站连接");
-                if (existRule == false)
+                else
                 {
-                    var blockedRule = FirewallManager.Instance.CreateApplicationRule(
-                                       FirewallManager.Instance.GetProfile().Type, @"阻止 " + ruleNameComponent + " 出站连接",
-                                       FirewallAction.Block, fileName);
-                    blockedRule.Direction = FirewallDirection.Outbound;
-                    blockedRule.Protocol = FirewallProtocol.Any;
-                    FirewallManager.Instance.Rules.Add(blockedRule);
+                    var currentProfiles = fwPolicy2.CurrentProfileTypes;
+                    firewallRule.Enabled = true;
+                    firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+                    firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+                    firewallRule.ApplicationName = fileName;
+                    firewallRule.Name = "阻止 " + ruleNameComponent + " 出站连接";
+                    firewallRule.Profiles = currentProfiles;
+                    operatePolicy.Rules.Add(firewallRule);
                 }
             }
             ReadAllFirewallRules();
+            displayNotification = true;
             ToastNotificationManagerCompat.History.Clear();
         }
 
@@ -166,7 +171,7 @@ namespace WindowsFirewallOutboundNotifier
                     {
                         throw new Win32Exception(Marshal.GetLastWin32Error(), "Call to QueryDosDevice failed!");
                     }
-                    diskMap.Add(sb.ToString().ToLower() + "\\", trimmedDrive); //FIXME: Switch to ToUpper?
+                    diskMap.Add(sb.ToString().ToLower() + "\\", trimmedDrive);
                 }
             }
             catch (Exception e)
